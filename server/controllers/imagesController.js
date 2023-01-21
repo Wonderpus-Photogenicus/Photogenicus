1864392fed3e312cf5cb9edd62c50b03e2634680
Dillon <93858218+d-hp@users.noreply.github.com>
2023-01-21 14:34:16 -0500

    Express routing boilerplate

---
+++ server/controllers/imagesController.js
@@ -0,0 +1,55 @@
+// require SQL dependency that allows us to query database i.e. People
+
+// imagesController will contain numerous methods that will be utilized within route handlers to handle required functionality
+const imagesController = {};
+
+// This middleware will be utilized when the client makes a post request to the server with an image to test
+imagesController.uploadImage = (req, res, next) => {
+  // Deconstructs req.body object for `imageUpload` that will contain an image uploaded by our client
+  // const { imageUpload } = req.body;
+  const { file } = req.files; // Using express-fileUpload package
+
+  // Image should be stored in `res.locals.imageToAuthenticate`
+  res.locals.imageToAuthenticate = imageUpload;
+
+  // Return the next middleware
+  return next();
+};
+
+// This middleware will be utilized when the admin makes a batch upload request to the server with images of the new NYOI cohort
+imagesController.batchImageUpload = (req, res, next) => {
+  // Deconstructs req.body object for images included within batch-upload
+  const { batchImages } = req.body;
+
+  // All images should be stored in SQL database
+
+  // Define SQL query/value parameters
+  const query = 'INSERT INTO people ... $1 ...';
+  const values = [batchImages];
+
+  // Execute SQL query to DB
+  db.query(query, values)
+    .then((dbResponse) => next())
+    .catch((err) => next({ message: `${err}` }));
+
+  // Continue to next middleware
+  return next();
+};
+
+// Upon client request for authentication of an image, this middleware should be activated to retrieve all photos from `people` SQL database.
+imagesController.retrieveImages = (req, res, next) => {
+  // Define SQL query parameters to retrieve all images currently stored in database
+  const query = 'SELECT image FROM images';
+
+  // Execute SQL query to DB
+  db.query(query, values)
+    // Store response of db into res.locals for access in next middleware
+    .then((peopleInDb) => {
+      res.locals.peopleInDb = peopleInDb;
+      return next();
+    })
+    .catch((err) => next({ message: `${err}` }));
+};
+
+// Export `imagesController` for use within `routes/images.js`
+module.exports = imagesController;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,52 @@
+const config = require('./utils/config.js');
 const path = require('path');
+const fileUpload = require('express-fileupload');
 const express = require('express');
-
 const app = express();
-const PORT = 3001;
+const morgan = require('morgan');
+const cors = require('cors');
 
-app.use(express.json())
+// imagesRouter handles all client requests to '/image' endpoint
+const imagesRouter = require('./routes/images.js');
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+// Defines cors middleware configuration
+const corsOptions = {
+  credentials: true,
+  origin: (origin, callback) => {
+    callback(null, true);
+  },
+};
+
+// Enabling middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(fileUpload());
+app.use(morgan('tiny'));
+
+// imagesRouter handles all client requests to '/image' endpoint
+app.use('/image', imagesRouter);
+
+// Unknown endpoint handler
+app.use('*', (req, res) =>
+  res.status(404).send("This is not the page you're looking for...")
+);
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
+// Listen to server on specified port (defined within config.js)
+app.listen(config.PORT, () => {
+  console.log(`Server listening on port: ${config.PORT}`);
+});
+
+// Export express app
 module.exports = app;
