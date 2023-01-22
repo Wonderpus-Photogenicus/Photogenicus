cf22841ae0a4195079053d44128a108ebdda35fc
JordanMcLuvin <jcepeda94@gmail.com>
2023-01-22 03:38:21 -0500

    Created database and conected it to the server. Defined database query middleware functions in the imageController file including batchImageUpload and retrieveImages. Left a clarification note on line 34 of imageController.js and notes on 'buffer to base64' on line 62.

--- server/controllers/imagesController.js
+++ server/controllers/imagesController.js
@@ -1,23 +1,14 @@
 // require SQL dependency that allows us to query database i.e. People
-
+const fs = require('fs');
+const path = require('path');
 const db = require('../models/image')
 
-// Baseplate test for database
-
-const addTest2 = db.prepare("INSERT INTO people (personName, image) VALUES (?, ?);");
-addTest2.run("superhot", "delete");
-
-const getTable = db.prepare("SELECT * FROM people");
-console.log(getTable.all());
 
 // imagesController will contain numerous methods that will be utilized within route handlers to handle required functionality
 const imagesController = {};
 
 // This middleware will be utilized when the client makes a post request to the server with an image to test
 imagesController.uploadImage = (req, res, next) => {
-
-  const addRow = db.prepare("INSERT INTO people (personName, image) VALUES (?, ?);");
-  addRow.run("superhot", "delete");
 
   //Webcam photo and headshot/reference file uploads will come in different spots
   //multi-file uplaods currently not working for some reason? but one phot will always upload here
@@ -28,8 +19,6 @@ imagesController.uploadImage = (req, res, next) => {
 
   // Deconstructs req.body object for `imageUpload` that will contain an image uploaded by our client
   // const { imageUpload } = req.body;
-  // Jordan: The image will be on the request body as a buffer. This line parses the buffer into a blob file.
-  const imageToUpload = new Blob([req.body]);
   const { file } = req.files; // Using express-fileUpload package
 
   // Image should be stored in `res.locals.imageToAuthenticate`
@@ -41,37 +30,43 @@ imagesController.uploadImage = (req, res, next) => {
 
 // This middleware will be utilized when the admin makes a batch upload request to the server with images of the new NYOI cohort
 imagesController.batchImageUpload = (req, res, next) => {
-  // Deconstructs req.body object for images included within batch-upload
+
+  // Clarification note: This feature will take individual submissions of photos. Mutiple image submissions could run multiple requests from the front end using for loops.
+  // The other option is for batch uploads of images to be merged into 1 mass image collage.
+  // For the second option: a table with columns for orginization name, and batchimage blob file would be more appropriate.
+  
+  // Deconstructs req.body object for images included within batch-upload. Batch images should be recieved as a buffer anyway allowing direct input into tables.
   const { batchImages } = req.body;
 
-  // All images should be stored in SQL database
+  // executes SQL query. failure of statement.run() method throws an error which will be caught and sent to the global error handler
+  try {
+    // person is a placeholder for names in the future. For the time being, names do not matter
+    const row = addRow.run('person', batchImages);
+  } catch(err){
+    return next({ log: err, message: 'database query failure in batchImageUpload'});
+  }
 
-  // Define SQL query/value parameters
-  const query = 'INSERT INTO people ... $1 ...';
-  const values = [batchImages];
-
-  // Execute SQL query to DB
-  db.query(query, values)
-    .then((dbResponse) => next())
-    .catch((err) => next({ message: `${err}` }));
-
-  // Continue to next middleware
   return next();
 };
 
 // Upon client request for authentication of an image, this middleware should be activated to retrieve all photos from `people` SQL database.
 imagesController.retrieveImages = (req, res, next) => {
-  // Define SQL query parameters to retrieve all images currently stored in database
-  const query = 'SELECT image FROM images';
 
-  // Execute SQL query to DB
-  db.query(query, values)
+  // Define SQL query parameters to retrieve all images currently stored in database
+  const getTable = db.prepare("SELECT image FROM people");
+
+  // Execute SQL query to DB. If query fails, catch the Error that is thrown by sqlite3 and throw it to global error hanlder.
+  try {
+    const peopleImages = getTable.all();
+
+    // If necessary buffers can be converted to base 64 here using: Buffer.from().toString('base64'));
+    // That command coupled with an array/object to push all the values into should work.
+    
     // Store response of db into res.locals for access in next middleware
-    .then((peopleInDb) => {
-      res.locals.peopleInDb = peopleInDb;
-      return next();
-    })
-    .catch((err) => next({ message: `${err}` }));
+    res.locals.peopleInDb = peopleImages;
+  } catch(err){
+    return next({ log: err, message: 'database query failure in retrieveImages'});
+  }
 };
 
 // Export `imagesController` for use within `routes/images.js`
